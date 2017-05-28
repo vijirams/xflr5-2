5f2815094563c017e622f886916f85393478d798
techwinder <techwinder@71f4f73e-3029-49f6-977e-f6bbc94eda0c>
2017-05-28 14:42:13 +0000

    - updating version 6.37

--- xflr5/xflr5-gui/gui_params.h
+++ xflr5/xflr5-gui/gui_params.h
@@ -12,7 +12,7 @@
 #define GUI_PARAMS_H
 
 
-#define VERSIONNAME     "xflr5 v6.36"
+#define VERSIONNAME     "xflr5 v6.37"
 
 //General
 #define MAXRECENTFILES         8  /**< Defines the maximum number of file names in the recent file list */
