6d5710224fd3ba97512da41974f7bf0334f796c6
techwinder <techwinder@71f4f73e-3029-49f6-977e-f6bbc94eda0c>
2018-11-16 14:22:18 +0000

    - corrected minor errors in the derotate operation of airfoils

--- xflr5/xflr5-gui/xdirect/geometry/FoilGeomDlg.h
+++ xflr5/xflr5-gui/xdirect/geometry/FoilGeomDlg.h
@@ -31,6 +31,8 @@
 #include <misc/text/DoubleEdit.h>
 #include <objects/objects2d/Foil.h>
 
+class XFoil;
+
 class FoilGeomDlg : public QDialog
 {
 	Q_OBJECT
@@ -68,11 +70,11 @@ private:
 	DoubleEdit	*m_pctrlThickness;
 	DoubleEdit	*m_pctrlCamber;
 
-	QPushButton *OKButton, *CancelButton, *RestoreButton;
+	QPushButton *m_OKButton, *m_CancelButton, *m_pRestoreButton;
 
 
 private:
-	static void* s_pXFoil;
+    static XFoil* s_pXFoil;
 
 	double m_fCamber;
 	double m_fThickness;
