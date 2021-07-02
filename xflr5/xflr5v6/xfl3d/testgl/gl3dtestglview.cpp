9025396bbed9c35c406c93f1f749b1e0381fd279
techwinder <techwinder@71f4f73e-3029-49f6-977e-f6bbc94eda0c>
2021-07-02 04:55:59 +0000

    code cleaning

---
+++ xflr5/xflr5v6/xfl3d/testgl/gl3dtestglview.cpp
@@ -0,0 +1,56 @@
+/****************************************************************************
+
+    gl3dTestView Class
+    Copyright (C) André Deperrois
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*****************************************************************************/
+
+#include <QOpenGLFunctions>
+#include <QOpenGLContext>
+#include <QOpenGLVertexArrayObject>
+
+#include "gl3dtestglview.h"
+#include <xfl3d/controls/w3dprefsdlg.h>
+
+gl3dTestGLView::gl3dTestGLView(QWidget *pParent) : gl3dView (pParent)
+{
+    setWindowTitle("Test GL");
+    m_bInitialized = false;
+}
+
+
+void gl3dTestGLView::glRenderView()
+{
+    double theta = 2.0*PI/3.0;
+    paintSphere(Vector3d(        0.0,         0.0, 0.0), 0.2, Qt::lightGray,  true);
+    paintSphere(Vector3d(        1.0,         0.0, 0.0), 0.1, Qt::darkGreen,  true);
+    paintSphere(Vector3d( cos(theta),  sin(theta), 0.0), 0.1, Qt::darkYellow, true);
+    paintSphere(Vector3d( cos(theta), -sin(theta), 0.0), 0.1, Qt::darkBlue,   true);
+    paintCube(0.0, 0.0, 0.5, 0.25, Qt::darkRed, true);
+
+    if (!m_bInitialized) {
+        m_bInitialized = true;
+        emit ready();
+    }
+}
+
+
+void gl3dTestGLView::on3dReset()
+{
+    startResetTimer(1.0);
+}
+
---
+++ xflr5/xflr5v6/xfl3d/testgl/gl3dtestglview.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+
+    gl3dTestView Class
+    Copyright (C) André Deperrois
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*****************************************************************************/
+
+#pragma once
+
+#include <xfl3d/gl3dview.h>
+#include <xflgeom/geom3d/vector3d.h>
+
+class gl3dTestGLView : public gl3dView
+{
+    Q_OBJECT
+
+    public:
+        gl3dTestGLView(QWidget *pParent = nullptr);
+        void glRenderView() override;
+        bool intersectTheObject(Vector3d const &,  Vector3d const &, Vector3d &) override {return false;}
+
+        void glMake3dObjects() override {}
+
+    signals:
+        void ready() const;
+
+    public slots:
+        void on3dReset() override;
+
+    private:
+        bool m_bInitialized;
+
+};
+
